1d9bfe54b1d3bbe48bc44e70924a987514d87ffb
ZSR <2472990740@qq.com>
2024-06-19 00:50:41 +0800

    feat: 实现了数据宽度自适应，大差距和小差距也能正常表现

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,33 @@
 import { Bar } from "@/components/Bar/Bar";
+import { Card } from "@tremor/react";
 
 export default function Home({}) {
   return (
     <>
       <div className="flex w-1/2 flex-col space-y-10">
-        <h1 className="text-lg text-white">有上下限</h1>
-        <Bar max={17.4} min={12.5} cursor={14} />
-        <Bar max={25} min={26} cursor={25.1} />
+        <h1 className="text-lg text-white">中等差距 不超出边界</h1>
+        <Card>
+          <Bar max={17.4} min={12.5} cursor={14} />
+        </Card>
 
-        <Bar max={30} min={10} cursor={39} />
+        <h1 className="text-lg text-white">微小差距 不超出边界</h1>
+        <Card>
+          <Bar max={26} min={25} cursor={25.1} />
+        </Card>
+
+        <h1 className="text-lg text-white">大差距 不超出边界</h1>
+        <Card>
+          <Bar max={30} min={10} cursor={39} />
+        </Card>
+
+        <h1 className="text-lg text-white">微小差距 超出边界</h1>
+        <Card>
+          <Bar max={3} min={2} cursor={4} />
+        </Card>
+
+        <Card>
+          <Bar max={3} min={2} cursor={10} />
+        </Card>
       </div>
     </>
   );
--- src/components/Bar/Bar.tsx
+++ src/components/Bar/Bar.tsx
@@ -1,8 +1,8 @@
-import { Card } from "@tremor/react";
-
 function formatNumberToPrecision(value: number, decimals: number) {
-  let factor = Math.pow(10, decimals);
-  return Math.round(value * factor) / factor;
+  if (value < 0) {
+    return "极小";
+  }
+  return value.toFixed(decimals);
 }
 
 export function Bar({
@@ -19,57 +19,96 @@ export function Bar({
   const gap = (max - min) / 4;
   const percentage = ((cursor - (min - 2 * gap)) / (gap * 8)) * 100;
 
+  const isOverrun = cursor > gap * 2 + max || cursor < min - gap * 2;
+  const percentagePerCount = Math.abs(40 / (cursor - (max + min) / 2));
+
   return (
     <>
-      <Card>
-        <div className="relative mt-6 flex h-2 w-full rounded-full bg-red-500">
-          <div className="relative h-2 w-[12.5%] rounded-s-full bg-orange-500">
-            <span className="absolute -left-3 -top-5 text-xs text-[#70757e]">
-              {formatNumberToPrecision(min - gap * 2, precision)}
-            </span>
-          </div>
-          <div className="relative h-2 w-[12.5%] bg-yellow-500">
-            <span className="absolute -left-3 -top-5 text-xs text-[#70757e]">
-              {formatNumberToPrecision(min - gap, precision)}
-            </span>
-          </div>
-          <div className="relative h-2 w-1/2 bg-green-500">
-            <span className="absolute -left-3 -top-5 text-xs text-[#70757e]">
-              {formatNumberToPrecision(min, precision)}
-            </span>
-          </div>
-          <div className="relative h-2 w-[12.5%] bg-yellow-500">
-            <span className="absolute -left-3 -top-5 text-xs text-[#70757e]">
-              {formatNumberToPrecision(max, precision)}
-            </span>
-          </div>
-          <div className="relative h-2 w-[12.5%] rounded-e-full bg-orange-500">
-            <span className="absolute -left-3 -top-5 text-xs text-[#70757e]">
-              {formatNumberToPrecision(max + gap, precision)}
-            </span>
-          </div>
-          <span className="absolute -right-3 -top-5 text-xs text-[#70757e]">
-            {formatNumberToPrecision(max + 2 * gap, precision)}
+      <div className="relative mt-6 flex h-2 w-full items-center justify-center rounded-full bg-red-500">
+        <div
+          className="relative h-2 rounded-s-full bg-orange-500"
+          style={{
+            width: !isOverrun
+              ? "12.5%"
+              : `${(100 - 25 - (max - min) * percentagePerCount) / 2}%`,
+          }}
+        >
+          <span className="absolute -left-3 -top-5 text-xs text-[#70757e]">
+            {formatNumberToPrecision(
+              !isOverrun
+                ? min - gap * 2
+                : (min + max) / 2 - 50 / percentagePerCount,
+              precision,
+            )}
           </span>
-
-          <i
-            className="group absolute -top-1 box-content h-4 w-1 cursor-pointer rounded-full outline outline-[3px]"
-            style={{
-              left: `calc(${percentage}%)`,
-              backgroundColor:
-                percentage < 12.5 || percentage > 87.5
-                  ? "rgb(249 115 22)"
-                  : percentage < 25 || percentage > 75
-                    ? " rgb(234 179 8)"
-                    : "rgb(34 197 94)",
-            }}
-          >
-            <div className="absolute -left-7 -top-10 hidden items-center justify-center rounded-lg border border-[#797a7c72] bg-white px-4 py-1 not-italic text-[#70757e] shadow-lg group-hover:flex">
-              {formatNumberToPrecision(cursor, precision)}
-            </div>
-          </i>
         </div>
-      </Card>
+        <div className="relative h-2 w-[12.5%] bg-yellow-500">
+          <span className="absolute -left-3 -top-5 text-xs text-[#70757e]">
+            {formatNumberToPrecision(
+              !isOverrun ? min - gap : min - 12.5 / percentagePerCount,
+              precision,
+            )}
+          </span>
+        </div>
+        <div
+          className="relative h-2 bg-green-500"
+          style={{
+            width: !isOverrun ? "50%" : `${percentagePerCount * (max - min)}%`,
+          }}
+        >
+          <span className="absolute -left-3 -top-5 text-xs text-[#70757e]">
+            {formatNumberToPrecision(min, precision)}
+          </span>
+        </div>
+        <div className="relative h-2 w-[12.5%] bg-yellow-500">
+          <span className="absolute -left-3 -top-5 text-xs text-[#70757e]">
+            {formatNumberToPrecision(max, precision)}
+          </span>
+        </div>
+        <div
+          className="relative h-2 rounded-e-full bg-orange-500"
+          style={{
+            width: !isOverrun
+              ? "12.5%"
+              : `${(100 - 25 - (max - min) * percentagePerCount) / 2}%`,
+          }}
+        >
+          <span className="absolute -left-3 -top-5 text-xs text-[#70757e]">
+            {formatNumberToPrecision(
+              !isOverrun ? max + gap : max + 12.5 / percentagePerCount,
+              precision,
+            )}
+          </span>
+        </div>
+
+        <span className="absolute -right-3 -top-5 text-xs text-[#70757e]">
+          {formatNumberToPrecision(
+            !isOverrun
+              ? max + 2 * gap
+              : max +
+                  (50 - (max - min) / percentagePerCount / 2) /
+                    percentagePerCount,
+            precision,
+          )}
+        </span>
+
+        <i
+          className="group absolute -top-1 box-content h-4 w-1 cursor-pointer rounded-full outline outline-[3px]"
+          style={{
+            left: !isOverrun ? `${percentage}%` : cursor > max ? "90%" : "10%",
+            backgroundColor:
+              percentage < 12.5 || percentage > 87.5
+                ? "rgb(249 115 22)"
+                : percentage < 25 || percentage > 75
+                  ? " rgb(234 179 8)"
+                  : "rgb(34 197 94)",
+          }}
+        >
+          <div className="absolute -left-7 -top-10 hidden items-center justify-center rounded-lg border border-[#797a7c72] bg-white px-4 py-1 not-italic text-[#70757e] shadow-lg group-hover:flex">
+            {formatNumberToPrecision(cursor, precision)}
+          </div>
+        </i>
+      </div>
     </>
   );
 }
