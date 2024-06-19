6e7e5bc391470244ec7058a95d6aaed359387730
ZSR <2472990740@qq.com>
2024-06-20 00:30:27 +0800

    feat: 实现了xlsx文件的读取

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,82 @@
-import { DoubleLimitBar, SingleLimitBar } from "@/components/Bar/";
-import { Card } from "@tremor/react";
+"use client";
 
-export default function Home({}) {
+import { DoubleLimitBar } from "@/components/Bar/";
+import { Card } from "@tremor/react";
+import { ChangeEventHandler, useState } from "react";
+import { read, utils } from "xlsx";
+
+export default function Home() {
+  const [jsonData, setJsonData] = useState<{ [key: string]: any }[]>([]);
+
+  const handleFileUpload: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const file = event.target.files![0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const data = new Uint8Array(e.target!.result as ArrayBuffer);
+      const workbook = read(data, { type: "array" });
+
+      // 只读取第一张表
+      const firstSheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[firstSheetName];
+
+      // 将工作表数据转换为 JSON
+      const sheetJson = utils.sheet_to_json(worksheet, { header: 1 });
+
+      // 处理数据：将标题行作为 key
+      const headers = sheetJson[0] as string[];
+      const rows = sheetJson.slice(1) as (string | number | undefined)[][];
+      const formattedData = rows.map((row) => {
+        const rowData: { [key: string]: any } = {};
+        headers.forEach((header, index) => {
+          rowData[header] = row[index];
+        });
+        return rowData;
+      });
+
+      setJsonData(formattedData);
+    };
+
+    reader.readAsArrayBuffer(file);
+  };
+
+  console.log(jsonData);
+
   return (
     <>
-      <div className="flex w-1/2 flex-col space-y-10">
-        <Card>
-          <div className="flex justify-between pb-5 text-lg text-black">
-            <p className="font-bold">成人外周血白细胞计数</p>
-            <p className="font-bold">
-              0.1<span className="pl-1 font-light">AU/ml</span>
-            </p>
-          </div>
+      <input type="file" onChange={handleFileUpload} />
+      <div className="flex h-full w-1/2 flex-col justify-center space-y-10">
+        <div className="flex h-4/5 flex-col gap-8 overflow-x-auto px-10">
+          {jsonData &&
+            (
+              jsonData as {
+                indicator_name: string;
+                result_value: number;
+                unit: string;
+                reference_value: string;
+              }[]
+            ).map((item, index) => (
+              <Card key={index}>
+                <div className="flex justify-between pb-5 text-lg text-black">
+                  <p className="font-bold">{item.indicator_name}</p>
+                  <p className="font-bold">
+                    {item.result_value}
+                    <span className="pl-1 font-light">
+                      {item.unit} {item.reference_value}
+                    </span>
+                  </p>
+                </div>
 
-          <DoubleLimitBar
-            max={9.5}
-            min={3.5}
-            cursor={0.1}
-            precision={1}
-            className="px-4"
-          />
-        </Card>
-
-        <Card>
-          <div className="flex justify-between pb-5 text-lg text-black">
-            <p className="font-bold">人体血小板</p>
-            <p className="font-bold">
-              100<span className="pl-1 font-light">AU/ml</span>
-            </p>
-          </div>
-
-          <DoubleLimitBar
-            max={30}
-            min={10}
-            cursor={100}
-            precision={1}
-            className="px-4"
-          />
-        </Card>
-
-        <Card>
-          <div className="flex justify-between pb-5 text-lg text-black">
-            <p className="font-bold">人体T淋巴数量</p>
-            <p className="font-bold">
-              30<span className="pl-1 font-light">AU/ml</span>
-            </p>
-          </div>
-
-          <SingleLimitBar min={10} cursor={30} precision={1} className="px-4" />
-        </Card>
-
-        <Card>
-          <div className="flex justify-between pb-5 text-lg text-black">
-            <p className="font-bold">人体B淋巴数量</p>
-            <p className="font-bold">
-              15<span className="pl-1 font-light">AU/ml</span>
-            </p>
-          </div>
-
-          <SingleLimitBar max={10} cursor={24} precision={1} className="px-4" />
-        </Card>
+                <DoubleLimitBar
+                  max={9.5}
+                  min={3.5}
+                  cursor={0.1}
+                  precision={1}
+                  className="px-4"
+                />
+              </Card>
+            ))}
+        </div>
       </div>
     </>
   );
